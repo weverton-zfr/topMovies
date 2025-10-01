c34b0eeed3cb8cbe5fc36ff0846a244bda43af0f
Weverton Ribeiro <wevertonribeiro009@gmail.com>
2025-10-01 20:17:19 -0300

    Conection with API

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,27 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
 const App = () => {
+  const [movies, setMovies] = useState([])
+  const url = 'https://api.themoviedb.org/3/discover/movie'
+  const key = '5bab60662c1c68f4433a9c985fc2300a'
+
+  const getMovies = () => {
+      axios({
+      method: 'get',
+      url: url,
+      params: {
+        api_key: key,
+        language: 'pt-BR'
+      }})
+      .then((data) => setMovies(data.data.results))
+      .catch((err) => console.error(err))
+    }
+console.log(movies)
+  useEffect(() => {
+    getMovies()
+  },[])
+
   return (
     <h1>Ola Mundo!</h1>
   )
